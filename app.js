c1012e9882f86175ce48e56b89d51535a01cd162
Wendy <lienyun.chang@gmail.com>
2022-06-20 09:50:16 +0800

    20220620 前後端合一

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const session = require('express-session');
 const connectFlash = require('connect-flash');
 const csrfProtection = require('csurf');
 const bodyParser = require('body-parser');
+const cors = require('cors');  
 
 
 // 第三個區塊 自建模組
@@ -34,18 +35,19 @@ const oneDay = 1000 * 60 * 60 * 24;
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({ 
-	secret: 'sessionToken',  // 加密用的字串
-	resave: false,   // 沒變更內容是否強制回存
-	saveUninitialized: false ,  // 新 session 未變更內容是否儲存
-	cookie: {
-		maxAge: oneDay // session 狀態儲存多久？單位為毫秒
-	}
-})); 
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: 'sessionToken',  // 加密用的字串
+    resave: false,   // 沒變更內容是否強制回存
+    saveUninitialized: false,  // 新 session 未變更內容是否儲存
+    cookie: {
+        maxAge: oneDay // session 狀態儲存多久？單位為毫秒
+    }
+}));
 app.use(connectFlash());
 // app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
+app.use(cors())
 
 app.use((req, res, next) => {
     res.locals.pageTitle = 'Book Your Books online';
@@ -60,7 +62,7 @@ app.use((req, res, next) => {
     }
     User.findByPk(req.session.user.id)
         .then((user) => {
-            console.log('postCartAddItem',user)
+            console.log('postCartAddItem', user)
             req.user = user;
             next();
         })
@@ -75,6 +77,15 @@ Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
 
+// 讀取 dist 資料
+// 如果路由是根目錄，則返回 Vue 的 index.html
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/dist/index.html`);
+});
+
+
+
 app.use(authRoutes);
 app.use(shopRoutes);
 app.use(errorRoutes);
@@ -82,14 +93,30 @@ app.use(errorRoutes);
 
 database
     .sync()
-	// .sync({force: true}) 
-	.then((result) => {
+    // .sync({force: true}) 
+    .then((result) => {
         // Product.bulkCreate(products);
-		app.listen(port, () => {
-			console.log(`Web Server is running on port ${port}`);
-		});
-	})
-	.catch((err) => {
-		console.log('create web server error: ', err);
-	});
+        app.listen(port, () => {
+            console.log(`Web Server is running on port ${port}`);
+        });
+    })
+    .catch((err) => {
+        console.log('create web server error: ', err);
+    });
+
+// var history = require('connect-history-api-fallback');
+// app.use(express.static(path.join(__dirname, '../dist')));
+// app.use(history());
+// // 解決跨域問題
+// const cors = require('cors');
+// app.use(cors({
+//     origin: ['http://localhost:8080'],
+//     methods: ['GET', 'POST'],
+// }));
+// app.all('*', function (req, res, next) {
+//     res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type');
+//     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+//     next();
+// });
 
